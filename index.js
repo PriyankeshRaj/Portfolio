ea13efb5f81ea8c06b48e4e535ae9f6fd50c5742
Priyankesh Raj <priyankeshraj@gmail.com>
2021-05-10 06:37:51 +0530

    Added Project section, contacts section, made more responsible

---
+++ index.js
@@ -0,0 +1,14 @@
+{
+	/* <div class="about-me-short">
+	<div class="designation">
+		<span>
+			<u>Developer</u>
+		</span>
+		<hr />
+	</div>
+	<div class="about-me-short-content">
+		Based in India, <br />
+		I'm a Computer Science Engineering Student
+	</div>
+</div>; */
+}
